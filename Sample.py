dc5ad63ccc329330f20f853c4abac9b89c3a1aaa
azimpa <muhammedasim.pa@gmail.com>
2024-10-17 21:29:33 +0530

    new

--- Sample.py
+++ Sample.py
@@ -1,20 +1,4 @@
-# Removing duplicates from a list without using a new list
-def remove_duplicates(lst):
-    seen = set()
-    i = 0
-    while i < len(lst):
-        if lst[i] in seen:
-            lst.pop(i)
-        else:
-            seen.add(lst[i])
-            i += 1
-
-
-# Example usage
-my_list = [1, 2, 3, 2, 1, 4, 5, 3]
-remove_duplicates(my_list)
-print(my_list)
-
-
-
-
+a_set = {51,6,7,2}
+b_set = {3, 4, 5, 2}
+union_set = a_set.symmetric_difference(b_set)
+print(union_set)
